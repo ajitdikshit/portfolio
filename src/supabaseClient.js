79cdbc3ce852fa2192c63a6c6915ddf92c8c5721
ajitdikshit <dikshitajit32@gmail.com>
2026-05-24 21:02:02 +0530

    Connected live Supabase database for dynamic projects

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://pgwiztqlvsvdjztszbhss.supabase.co'; // From your Project Settings > API
+const supabaseUrl = 'https://pgwiztqlvsvdjztszhss.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBnd2l6dHFsdnN2ZGp6dHN6aHNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2MjUyMjYsImV4cCI6MjA5NTIwMTIyNn0.b-fDmEvUGnbXY1kg4yGXvyCToDL2ZWTkCDWFyHctOFc'; // From your Project Settings > API
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
