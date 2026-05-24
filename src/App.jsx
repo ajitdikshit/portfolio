ea3f2d7f53d6f5042a4f9a52de2ecc5993ad28ff
ajitdikshit <dikshitajit32@gmail.com>
2026-05-24 20:09:29 +0530

    Added Google Sheets contact form and fixed stats

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-
+import { Analytics } from "@vercel/analytics/react"
 // --- Assets ---
 import profilePic from './portfolio.jpg';
 import hackathonMain from './hackathon2.jpg';
