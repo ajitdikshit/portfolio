8d2f61c6c8410fd990f6fa5724ee87d8a98a9548
ajitdikshit <dikshitajit32@gmail.com>
2026-05-25 19:52:25 +0530

    Added dial for day night mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { supabase } from './supabaseClient';
 import './App.css';
 import { Analytics } from "@vercel/analytics/react"
